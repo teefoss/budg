5c2766e79ed00f2e409bc53fb5de5ec3db542589
Thomas Foster <foster.pianist@gmail.com>
2025-02-23 11:44:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+`budg` is a super-simple cl budget program.
+
+Given an initial budget for the month, it tells you how much you should have left based on how far into the month it is. For example, on the 15th:
+
+```
+$ budg 2000
+Current funds should be 1000.
+```
+
+Optionally, if you also give it your the funds remaining, it tells you how far over or under budget you are, and how much you can spend per day without going over budget. For example, on the 15th:
+
+```
+$ budg 2000 1100
+Current funds should be 1000.
+You are under budget by 100.
+There are 15 days left; spend limit of 73 per day.
+```
---
+++ budg.c
@@ -0,0 +1,100 @@
+/*
+ * budg.c - Monthly Budget Progress Calcuator
+ *
+ * Copyright (C) 2025 Thomas Foster (www.github.com/teefoss)
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include <errno.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define VERSION "1.0"
+
+const int month_len[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+int parse_int(const char * str) {
+    char *endptr;
+    errno = 0;
+    long value = strtol(str, &endptr, 10);
+
+    if ( errno == ERANGE ) {
+        printf("Error: value out of range\n");
+        exit(1);
+    }
+
+    if ( endptr == str || *endptr != '\0' ) {
+        printf("Error: invalid number format\n");
+        exit(1);
+    }
+
+    return (int)value;
+}
+
+int main(int argc, char ** argv)
+{
+    if ( argc == 1 ) {
+        printf("Error: expected arguments.\n");
+        printf("Usage: %s [initial budget] (current funds)\n", argv[0]);
+        printf("       %s --info\n", argv[0]);
+        return 1;
+    }
+
+    if ( strcmp(argv[1], "--info") == 0 ) {
+        printf("%s Ver. %s -- Monthly Budget Progress Calculator\n",
+               argv[0], VERSION);
+        printf("Copyright (C) 2025 Thomas Foster (www.github.com/teefoss)\n");
+        return 0;
+    }
+
+    int funds = parse_int(argv[1]);
+    if ( funds == 0 ) {
+        printf("Error: bad budget value\n");
+        return 1;
+    }
+
+    time_t t = time(NULL);
+    struct tm *tm_info = localtime(&t);
+
+    int days_left = month_len[tm_info->tm_mon] - tm_info->tm_mday;
+    float progress = (float)days_left / (float)month_len[tm_info->tm_mon];
+
+    float expected = roundf((float)funds * progress);
+    printf("Current funds should be %.0f.\n", expected);
+
+    if ( argc == 3 ) {
+        float actual = (float)parse_int(argv[2]);
+        float diff = actual - expected;
+
+        if ( diff == 0.0f ) {
+            printf("You are on budget.\n");
+        } else {
+            printf("You are %s budget by %.0f.\n",
+                   diff < 0 ? "over" : "under", fabsf(diff));
+        }
+
+        printf("There are %d days left; spend limit of %.0f per day.\n",
+               days_left, actual / days_left);
+    }
+
+    return 0;
+}
